0d07977813195018a07be86c28363a48cda33df6
uyvunk <uyvunk@gmail.com>
2015-02-25 02:04:18 -0800

    Downloader be able to connect to MongoDB and get the coverImg url from it. The download it from the internet into a directory has the name of the app.

--- AppCrawlerProject/src/downloader.java
+++ AppCrawlerProject/src/downloader.java
@@ -25,58 +25,58 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 
 public class downloader {
-
+	private static String ROOT_DIR = "D:\\sampleScreenShots\\";
+	private static String COLLECTION_NAME = "ProcessedApps_2015", DB_NAME="admin",DB_USER_NAME="siteUserAdmin";
+	private static char[] DB_PASSWRD = {'p','a','s','s','w','o','r','d'};
 	private static MongoClient mongoClient;
+	
 
 	public static void main(String[] args) throws MalformedURLException {
-		char[] password = {'p','a','s','s','w','o','r','d'};
-		MongoCredential credential = MongoCredential.createMongoCRCredential("siteUserAdmin", "admin", password);
-		MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
-		//mongoClient = new MongoClient( "localhost" , 27017 );
-		DB db = mongoClient.getDB("admin");
-		DBCollection coll = db.getCollection("testData");
-		DBCursor cursor = coll.find();
+		//Initialize the connection to DB
+		MongoCredential credential = MongoCredential.createMongoCRCredential(DB_USER_NAME, DB_NAME, DB_PASSWRD);
+		mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+		DB db = mongoClient.getDB(DB_NAME);	//Open connection to DB_NAME
+		DBCollection coll = db.getCollection(COLLECTION_NAME);
+		//Create cursor to iterate the elements of the collection
+		DBCursor cursor = coll.find().limit(10); //Set the value of limit for testing purpose. Remove limit when ready to run
+		
+		//Set currentRand for app that doesn't have name
+		int currentRand=0;
 		try {
+			//BasicDBObject query = new BasicDBObject();
+	
 			while (cursor.hasNext()){
-				System.out.println(cursor.next());
+				DBObject tuple = cursor.next();
+				String strUrl = tuple.get("CoverImgUrl").toString(); // assigning CoverImgUrl to strUrl
+				String appName = tuple.get("Name").toString(); // assigning Name to appName
+				if (appName.isEmpty()){
+					appName="app "+currentRand;
+					currentRand++;
+				}
+				String path = ROOT_DIR + appName; //Initialize Path to file
+				URL appUrl = new URL(strUrl); 
+				
+				// Use Apache commons IO to create directory with the name appName, and download the image into it
+				File file = new File(path);
+				if (!file.exists()) {
+					if (file.mkdir()) {
+						System.out.println("Directory "+appName+" is created!");
+					} else {
+						System.out.println("Failed to create directory!");
+					}
+				}
+				File photo = new File(path+"\\photo.webp");
+				try {
+					FileUtils.copyURLToFile(appUrl, photo)	;
+				} catch (Exception e) {
+					System.out.println(e);
+				}
 			}
 		
 		} finally {
 			cursor.close();
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//Initialize Path to file
-		String strUrl = "https://lh6.ggpht.com/8rkB5fph8KfgpAM27k3lIODTLUpuPlMmP9W65ldZQNMUdSob591CVl4ztrdsIDiy_WwP=h900-rw";
-		URL appUrl = new URL(strUrl);
-		String appName = "app1";
-		String path = "Z:\\sampleScreenShots\\"+appName;
-		
-		File file = new File(path);
-		if (!file.exists()) {
-			if (file.mkdir()) {
-				System.out.println("Directory "+appName+" is created!");
-			} else {
-				System.out.println("Failed to create directory!");
-			}
-		}
-		File photo = new File(path+"\\photo.webp");
-		try {
-			FileUtils.copyURLToFile(appUrl, photo)	;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		mongoClient.close();
+		mongoClient.close();	//Close Connection to DB_NAME
 	}
 	
 	
