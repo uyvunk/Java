bf0d9b1d7a4b97d1cf47df2da94060b409d3c18d
uyvunk <uyvunk@gmail.com>
2015-02-24 20:32:40 -0800

    work on adding MongoDb

--- AppCrawlerProject/src/downloader.java
+++ AppCrawlerProject/src/downloader.java
@@ -2,11 +2,60 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
+
 import org.apache.commons.io.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+
 public class downloader {
 
+	private static MongoClient mongoClient;
+
 	public static void main(String[] args) throws MalformedURLException {
-		// TODO Auto-generated method stub
+		char[] password = {'p','a','s','s','w','o','r','d'};
+		MongoCredential credential = MongoCredential.createMongoCRCredential("siteUserAdmin", "admin", password);
+		MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+		//mongoClient = new MongoClient( "localhost" , 27017 );
+		DB db = mongoClient.getDB("admin");
+		DBCollection coll = db.getCollection("testData");
+		DBCursor cursor = coll.find();
+		try {
+			while (cursor.hasNext()){
+				System.out.println(cursor.next());
+			}
+		
+		} finally {
+			cursor.close();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//Initialize Path to file
 		String strUrl = "https://lh6.ggpht.com/8rkB5fph8KfgpAM27k3lIODTLUpuPlMmP9W65ldZQNMUdSob591CVl4ztrdsIDiy_WwP=h900-rw";
 		URL appUrl = new URL(strUrl);
 		String appName = "app1";
@@ -27,7 +76,9 @@ public class downloader {
 			System.out.println(e);
 		}
 		
-
+		mongoClient.close();
 	}
+	
+	
 
 }
